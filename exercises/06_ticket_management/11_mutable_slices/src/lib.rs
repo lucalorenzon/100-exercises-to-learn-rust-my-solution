d9a18b2a519dfd5d6bfe2056b9d9426bce807bd3
Luca Lorenzon <1104352+lucalorenzon@users.noreply.github.com>
2026-09-26 06:28:47 +0200

    feat(06-ticket-management-11-mutable-slices): done

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,5 +1,9 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
+//
+pub fn squared(values: &mut [i32]) {
+    values.iter_mut().for_each(|item| *item *= *item);
+}
 
 #[cfg(test)]
 mod tests {
